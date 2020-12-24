7919b0d5a538fd00e322195df72b501acfb81bd0
Matthew Burke <matthewpatrickburke@gmail.com>
2020-12-24 15:59:34 -0700

    Create adonis and mithril projects

---
+++ frontend/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+
+module.exports = [
+  {
+    name: "frontend",
+    mode: "development",
+    entry: "./index.ts",
+    devtool: "inline-source-map",
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: "ts-loader",
+          exclude: /node_modules/,
+        },
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    resolve: {
+      extensions: [".tsx", ".ts", ".js"],
+    },
+    output: {
+      filename: "bundle.js",
+      path: path.resolve(__dirname, "../backend/public/bundle"),
+      publicPath: "/",
+    },
+  },
+];
