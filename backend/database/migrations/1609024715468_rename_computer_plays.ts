edd1b18869daa226785e24423504339d4e28d739
Matthew Burke <matthewpatrickburke@gmail.com>
2020-12-27 12:13:35 -0700

    Show own pieces in fog

--- backend/database/migrations/1609024715468_rename_computer_plays.ts
+++ backend/database/migrations/1609024715468_rename_computer_plays.ts
@@ -11,7 +11,7 @@ export default class LiveGames extends BaseSchema {
 
   public async down() {
     this.schema.table(this.tableName, (table) => {
-      table.dropColumn("computerPlays");
+      table.dropColumn("computer_plays");
     });
   }
 }
